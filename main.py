77eaf151d6aaf64cee0908dcd44a4b1109225ff4
korpaxdev <korpaxdev@gmail.com>
2023-01-23 13:13:23 +0300

    lesson_3

--- main.py
+++ main.py
@@ -1,27 +1,21 @@
 from flat_iterator import FlatListIterator
-from flat_gen import flat_list_gen
 
 
-def run():
-    nested_list = [
-        ['a', ['list', 1, ['new_list']], 'b', 'c'],
-        ['d', 'e', 'f', 'h', False],
-        [1, 2, None, ['deep_list', 333, ['again_list', [555]]]],
-        12
+def test_3():
+    list_of_lists_2 = [
+        [['a'], ['b', 'c']],
+        ['d', 'e', [['f'], 'h'], False],
+        [1, 2, None, [[[[['!']]]]], []]
     ]
-    print('[ITERATOR ELEMENTS]:')
-    for i in FlatListIterator(nested_list):
-        print(i)
-    print()
-    print('[GENERATOR ELEMENTS]:')
-    for i in flat_list_gen(nested_list):
-        print(i)
-    flat_list_1 = [i for i in FlatListIterator(nested_list)]
-    flat_list_2 = [i for i in flat_list_gen(nested_list)]
-    print()
-    print('[FROM ITERATOR]:', flat_list_1)
-    print('[FROM GENERATOR]:', flat_list_2)
+
+    for flat_iterator_item, check_item in zip(
+            FlatListIterator(list_of_lists_2),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+    ):
+        assert flat_iterator_item == check_item
+
+    assert list(FlatListIterator(list_of_lists_2)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
 
 
 if __name__ == '__main__':
-    run()
+    test_3()
